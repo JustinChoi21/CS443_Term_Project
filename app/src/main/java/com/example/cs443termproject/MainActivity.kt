64b2e42644de6f0726f27649a6e1f63cb3687485
heestory21@gmail.com <heestory21@gmail.com>
2022-11-01 11:33:47 -0400

    first commit

---
+++ app/src/main/java/com/example/cs443termproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cs443termproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main) // this will be deprecated and replaced by ViewBinding
+    }
+}
