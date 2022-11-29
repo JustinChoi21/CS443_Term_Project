bf324ea4566fc4f531e64afc462b2430a8de2581
JustinChoi21 <kodingcat21@gmail.com>
2022-11-29 07:23:55 -0500

    feat: HistoryItemAdapter

--- app/src/main/java/edu/umb/cs443termproject/adapter/HistoryItemAdapter.kt
+++ app/src/main/java/edu/umb/cs443termproject/adapter/HistoryItemAdapter.kt
@@ -1,9 +1,32 @@
 package edu.umb.cs443termproject.adapter
 
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import edu.umb.cs443termproject.R
 import edu.umb.cs443termproject.data.HistoryItems
 
-class HistoryItemAdapter(val historyList: ArrayList<HistoryItems>) : RecyclerView.Adapter<HistoryItemAdapter.CustomViewHolder>()
-{
+class HistoryItemAdapter(val historyList: ArrayList<HistoryItems>) : RecyclerView.Adapter<HistoryItemAdapter.CustomViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    class CustomViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val icon = itemView.findViewById<ImageView>(R.id.iv_list_icon_history)
+        val eventType = itemView.findViewById<TextView>(R.id.tv_event_type)
+        val
+
+    }
 
 }
