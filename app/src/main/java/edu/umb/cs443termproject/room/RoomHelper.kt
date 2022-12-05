ca35ca63cf62efa827bc0254fdc0c8efa0c623df
heestory21@gmail.com <heestory21@gmail.com>
2022-12-05 08:37:35 -0500

    fix: Room version upgrade

--- app/src/main/java/edu/umb/cs443termproject/room/RoomHelper.kt
+++ app/src/main/java/edu/umb/cs443termproject/room/RoomHelper.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = arrayOf(Login::class, Car::class), version = 1, exportSchema = false)
+@Database(entities = arrayOf(Login::class, Car::class), version = 2, exportSchema = false)
 abstract class RoomHelper : RoomDatabase() {
 
     // DB table list
