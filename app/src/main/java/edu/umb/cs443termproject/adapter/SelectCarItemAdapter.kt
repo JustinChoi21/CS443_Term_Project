471b613c8f75950b287195af3871686ba63b15c2
heestory21@gmail.com <heestory21@gmail.com>
2022-12-05 09:18:54 -0500

    feat: store selected car info & move to homeFragment

--- app/src/main/java/edu/umb/cs443termproject/adapter/SelectCarItemAdapter.kt
+++ app/src/main/java/edu/umb/cs443termproject/adapter/SelectCarItemAdapter.kt
@@ -1,19 +1,30 @@
 package edu.umb.cs443termproject.adapter
 
 import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat.startActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.RecyclerView
 import edu.umb.cs443termproject.MainActivity
 import edu.umb.cs443termproject.R
 import edu.umb.cs443termproject.data.SelectCarItems
+import edu.umb.cs443termproject.fragments.HomeFragment
+import edu.umb.cs443termproject.room.Car
+import edu.umb.cs443termproject.room.RoomHelper
+import kotlinx.coroutines.launch
 
 class SelectCarItemAdapter(val selectCarList: ArrayList<SelectCarItems>) : RecyclerView.Adapter<SelectCarItemAdapter.CustomViewHolder>() {
+
+    companion object {
+        const val TAG: String = "CS443"
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SelectCarItemAdapter.CustomViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_select_car, parent, false)
@@ -43,7 +54,25 @@ class SelectCarItemAdapter(val selectCarList: ArrayList<SelectCarItems>) : Recyc
         holder.manufacturer_model.text =
             selectCarList.get(position).ManufacturerName + " " + selectCarList.get(position).model
 
-        // store car info & move to home
+        // store selected car info & move to homeFragment
+        holder.itemView.setOnClickListener(object :View.OnClickListener{
+            override fun onClick(v: View?) {
+                Log.d(TAG, "onClick: holder.itemView.setOnClickListener")
+
+                // move to homeFragment
+                val activity = v!!.context as AppCompatActivity
+
+                // store selected car info
+                activity.lifecycleScope.launch{
+                    val car: Car = Car(selectCarList.get(position).ManufacturerName, selectCarList.get(position).model)
+                    RoomHelper.getDatabase(activity).getCarDao().addCar(car)
+                }
+
+                val homeFragment = HomeFragment.newInstance()
+                activity.supportFragmentManager.beginTransaction().replace(R.id.fragments_frame, homeFragment)
+                    .addToBackStack(null).commit()
+            }
+        })
 
     }
 
