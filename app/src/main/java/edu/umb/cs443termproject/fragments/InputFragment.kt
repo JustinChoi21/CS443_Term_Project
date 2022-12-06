3206a9f5ea8317757228d72803da7bde995db115
JustinChoi21 <kodingcat21@gmail.com>
2022-12-06 17:13:51 -0500

    feat: input drop down & login page style

--- app/src/main/java/edu/umb/cs443termproject/fragments/InputFragment.kt
+++ app/src/main/java/edu/umb/cs443termproject/fragments/InputFragment.kt
@@ -5,8 +5,10 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import edu.umb.cs443termproject.R
+import edu.umb.cs443termproject.databinding.FragmentInputBinding
 
 class InputFragment : Fragment() {
 
@@ -18,12 +20,31 @@ class InputFragment : Fragment() {
         }
     }
 
+    private var _binding: FragmentInputBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         Log.d(TAG, "HomeFragment - onCreateView() called")
-        return inflater.inflate(R.layout.fragment_input, container, false)
+
+        _binding = FragmentInputBinding.inflate(inflater, container, false)
+
+        val eventTypes = resources.getStringArray(R.array.stringArray_eventType)
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_event_type_item, eventTypes)
+        binding.autoTvDropdownEventType.setAdapter(arrayAdapter)
+
+        return binding.root
+    }
+
+    // recreate drop-down when comeback to app
+    override fun onResume() {
+        super.onResume()
+
+        val eventTypes = resources.getStringArray(R.array.stringArray_eventType)
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_event_type_item, eventTypes)
+        binding.autoTvDropdownEventType.setAdapter(arrayAdapter)
     }
 }
