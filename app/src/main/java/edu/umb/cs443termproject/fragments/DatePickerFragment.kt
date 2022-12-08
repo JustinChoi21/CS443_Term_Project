091d82b181fb3dc5ab721ee43668a23d99c20a9f
JustinChoi21 <kodingcat21@gmail.com>
2022-12-08 07:56:52 -0500

    feat: date picker dialog

--- app/src/main/java/edu/umb/cs443termproject/fragments/DatePickerFragment.kt
+++ app/src/main/java/edu/umb/cs443termproject/fragments/DatePickerFragment.kt
@@ -5,14 +5,26 @@ import android.app.Dialog
 import android.os.Bundle
 import android.widget.DatePicker
 import androidx.fragment.app.DialogFragment
+import edu.umb.cs443termproject.MainActivity
 import java.util.*
 
 class DatePickerFragment: DialogFragment(), DatePickerDialog.OnDateSetListener {
 
-    private val calendar = Calendar.getInstance()
-
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return super.onCreateDialog(savedInstanceState)
+        // Use the current date as the default date in the picker
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        // Create a new instance of DatePickerDialog and return it
+        return DatePickerDialog(
+            requireActivity(),
+            this,
+            year,
+            month,
+            day
+        )
     }
 
     override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
