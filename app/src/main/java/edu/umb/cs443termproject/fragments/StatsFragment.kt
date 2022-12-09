8678aa339af8b5e0d17ac5c6ad0da708b5601c60
JustinChoi21 <kodingcat21@gmail.com>
2022-12-08 20:02:00 -0500

    feat: bar chart

--- app/src/main/java/edu/umb/cs443termproject/fragments/StatsFragment.kt
+++ app/src/main/java/edu/umb/cs443termproject/fragments/StatsFragment.kt
@@ -6,7 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
 import edu.umb.cs443termproject.R
+import kotlinx.android.synthetic.main.fragment_stats.*
 
 class StatsFragment : Fragment() {
 
@@ -25,5 +30,33 @@ class StatsFragment : Fragment() {
     ): View? {
         Log.d(HomeFragment.TAG, "RemindersFragment - onCreateView() called")
         return inflater.inflate(R.layout.fragment_stats, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d(HomeFragment.TAG, "RemindersFragment - onViewCreated() called")
+
+        // bar chart
+        val barChart = activity?.findViewById<BarChart>(R.id.barChart)
+        val list: ArrayList<BarEntry> = ArrayList()
+        list.add(BarEntry(1f, 10f))
+        list.add(BarEntry(2f, 20f))
+        list.add(BarEntry(3f, 30f))
+        list.add(BarEntry(4f, 40f))
+        list.add(BarEntry(5f, 50f))
+        list.add(BarEntry(6f, 60f))
+        list.add(BarEntry(7f, 70f))
+        list.add(BarEntry(8f, 80f))
+        list.add(BarEntry(9f, 90f))
+        list.add(BarEntry(10f, 100f))
+        list.add(BarEntry(11f, 110f))
+        list.add(BarEntry(12f, 120f))
+
+        val barDataSet = BarDataSet(list, "Fuel Consumption")
+        val data = BarData(barDataSet)
+        barChart?.data = data
+        barChart?.description?.text = "Bar Chart Example"
+        barChart?.animateY(200)
+
     }
 }
