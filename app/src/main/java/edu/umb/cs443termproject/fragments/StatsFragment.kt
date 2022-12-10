40b51cfcaa0a73958ed55da21351839bbeee54fc
JustinChoi21 <kodingcat21@gmail.com>
2022-12-09 22:21:05 -0500

    fix: bar chart setting

--- app/src/main/java/edu/umb/cs443termproject/fragments/StatsFragment.kt
+++ app/src/main/java/edu/umb/cs443termproject/fragments/StatsFragment.kt
@@ -7,9 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import edu.umb.cs443termproject.MainActivity
 import edu.umb.cs443termproject.R
 import kotlinx.android.synthetic.main.fragment_stats.*
@@ -44,37 +47,48 @@ class StatsFragment : Fragment() {
         // bar chart
         val barChart = activity?.findViewById<BarChart>(R.id.barChart)
         val list: ArrayList<BarEntry> = ArrayList()
-        list.add(BarEntry(1f, 10f))
-        list.add(BarEntry(2f, 20f))
-        list.add(BarEntry(3f, 30f))
-        list.add(BarEntry(4f, 40f))
-        list.add(BarEntry(5f, 50f))
-        list.add(BarEntry(6f, 60f))
-        list.add(BarEntry(7f, 70f))
-        list.add(BarEntry(8f, 80f))
-        list.add(BarEntry(9f, 90f))
-        list.add(BarEntry(10f, 100f))
-        list.add(BarEntry(11f, 110f))
-        list.add(BarEntry(12f, 120f))
+        list.add(BarEntry(0.5f, 10f))
+        list.add(BarEntry(1.5f, 20f))
+        list.add(BarEntry(2.5f, 30f))
+        list.add(BarEntry(3.5f, 40f))
+        list.add(BarEntry(4.5f, 50f))
+        list.add(BarEntry(5.5f, 60f))
+        list.add(BarEntry(6.5f, 70f))
+        list.add(BarEntry(7.5f, 80f))
+        list.add(BarEntry(8.5f, 90f))
+        list.add(BarEntry(9.5f, 100f))
+        list.add(BarEntry(10.5f, 110f))
+        list.add(BarEntry(11.5f, 120f))
 
         val barDataSet = BarDataSet(list, "Fuel Consumption (Gallon)")
         val data = BarData(barDataSet)
         barChart?.data = data
         barChart?.description?.text = ""
-        barChart?.setFitBars(true)
+        barChart?.setFitBars(false)
         barChart?.animateY(200)
 
         // barchart x axis
-        barChart?.xAxis?.setDrawLabels(true)
-        barChart?.xAxis?.setDrawAxisLine(true)
-        barChart?.xAxis?.setDrawGridLines(true)
-        barChart?.xAxis?.setDrawLimitLinesBehindData(true)
+        val xAxis: XAxis? = barChart?.xAxis
+        xAxis?.setDrawLabels(true)
+        xAxis?.setDrawAxisLine(true)
+        xAxis?.setDrawGridLines(true)
+        xAxis?.setDrawLimitLinesBehindData(true)
+
+        xAxis?.position = XAxis.XAxisPosition.BOTTOM
+
+        // set xAxis labels to months
+        val months = arrayOf("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+        xAxis?.valueFormatter = IndexAxisValueFormatter(months)
+        xAxis?.labelCount = months.size
+        xAxis?.setCenterAxisLabels(true)
 
         // barchart y axis
-        barChart?.axisLeft?.setDrawLabels(true)
-        barChart?.axisLeft?.setDrawAxisLine(true)
-        barChart?.axisLeft?.setDrawGridLines(true)
-        barChart?.axisLeft?.setDrawLimitLinesBehindData(true)
+        val yAxis: YAxis? = barChart?.axisLeft
+        yAxis?.setDrawLabels(true)
+        yAxis?.setDrawAxisLine(true)
+        yAxis?.setDrawGridLines(true)
+        yAxis?.setDrawLimitLinesBehindData(true)
+
 
     }
 }
