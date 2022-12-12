7de2b7fda72186cf3df8a5014914a0030431aa61
JustinChoi21 <kodingcat21@gmail.com>
2022-12-12 10:58:19 -0500

    fix: notification

--- app/src/main/java/edu/umb/cs443termproject/notifications/NotificationHelper.kt
+++ app/src/main/java/edu/umb/cs443termproject/notifications/NotificationHelper.kt
@@ -1,12 +1,47 @@
 package edu.umb.cs443termproject.notifications
 
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.content.ContextWrapper
 import android.os.Build
-import edu.umb.cs443termproject.MainActivity
+import androidx.core.app.NotificationCompat
 
-class NotificationHelper {
+class NotificationHelper(base: Context?) : ContextWrapper(base) {
 
+    // channel id and name
+    private val channelID = "CS443ChannelId"
+    private val channelName = "CS443ChannelName"
+
+    init {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createChannels()
+        }
+    }
+
+    private fun createChannels() {
+        val channel = NotificationChannel(channelID, channelName, NotificationManager.IMPORTANCE_DEFAULT)
+        channel.enableLights(true)
+        channel.enableVibration(true)
+        channel.lockscreenVisibility = Notification.VISIBILITY_PRIVATE
+
+        getManager().createNotificationChannel(channel)
+
+    }
+
+    // Create NotificationManager
+    fun getManager(): NotificationManager {
+        return getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+    }
+
+    // setting Notification
+    fun getChannelNotification(title: String, body: String): NotificationCompat.Builder {
+        return NotificationCompat.Builder(applicationContext, channelID)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setSmallIcon(android.R.drawable.ic_dialog_info)
+            .setAutoCancel(true)
+    }
 
 }
